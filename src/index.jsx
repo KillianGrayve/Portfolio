350c0b4af92dbe8549d92473e51e6a3682841e4f
Eric Lideme <Ericlideme.p@hotmail.com>
2023-03-10 03:51:16 -0300

    Update 3.1

--- src/index.jsx
+++ src/index.jsx
@@ -5,11 +5,12 @@ import './style/index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+function tick() {
     root.render(
         <React.StrictMode>
             <App />
         </React.StrictMode>
     );
+}
 
-
-
+setInterval(tick, 400);
